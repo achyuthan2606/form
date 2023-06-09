3e02a0c61755a4d87ef78eebab987d2978423602
achyuthan2606 <129186574+achyuthan2606@users.noreply.github.com>
2023-06-10 00:00:45 +0530

    Add files via upload

---
+++ Form.java
@@ -0,0 +1,16 @@
+package com.Form;
+
+public class Form {
+    
+}
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Form {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Form.class, args);
+	}
+
+}
